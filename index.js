0908833b07f53c7de9be1059281336b08d09b9c3
DuarteXV <danielalejandrobasado@gmail.com>
2026-04-04 20:48:33 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ return true
 }
 
 await global.reloadHandler()
-const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
+const pluginFolder = global.__dirname(join(__dirname, './plugins'))
 const pluginFilter = (filename) => /\.js$/.test(filename)
 global.plugins = {}
 
